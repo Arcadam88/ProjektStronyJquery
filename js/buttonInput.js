9567dc828a03b2a7738dcb49a80dba5d4be602ea
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2021-07-26 08:27:29 +0200

    Version 1.6.7

--- js/buttonInput.js
+++ js/buttonInput.js
@@ -1,98 +1,77 @@
-$(document).ready(function() //kiedy dokument będzie gotowy, załadowany funkcja może zostać załadowana
-{
-    $("#dlcButton").click(function()
+$(document).ready(function () //kiedy dokument będzie gotowy, załadowany funkcja może zostać załadowana
     {
-    $("#dlc").fadeIn(500); //wyświetla okno z elementem o id DLC
-    $("#info, #picture").fadeOut(500); //ukrywa okno z elementem o id info
-    }),
-    $("#infoButton").click(function()
-    {
-        $("#dlc, #picture").fadeOut(500);
-        $("#info").fadeIn(500);       
-    }),
-    $("#pictureButton").click(function()
-    {
-        $("#dlc, #dlcText, #info").fadeOut(500);
-        $("#picture").fadeIn(500);
+        $("#dlcButton").click(function () {
+                $("#dlc").fadeIn(500); //wyświetla okno z elementem o id DLC
+                $("#info, #picture").fadeOut(500); //ukrywa okno z elementem o id info
+            }),
+            $("#infoButton").click(function () {
+                $("#dlc, #picture").fadeOut(500);
+                $("#info").fadeIn(500);
+            }),
+            $("#pictureButton").click(function () {
+                $("#dlc, #dlcText, #info").fadeOut(500);
+                $("#picture").fadeIn(500);
+            });
     });
-});
 
-$(document).ready(function()
-{
-    $("#scrollUp").click(function()
-    {
-        $(window).scrollTop(0,0);
+$(document).ready(function () {
+    $("#scrollUp").click(function () {
+        $(window).scrollTop(0, 0);
     });
 });
 
 
 //button hiding or displaying button group with "games" button etc.
-$(document).ready(function() 
-{
+$(document).ready(function () {
     {
-       $(".btn-group").hide(); 
+        $(".btn-group").hide();
     }
-    $("#menu").click(function()
-    {
+    $("#menu").click(function () {
         $(".btn-group").fadeToggle(300);
     });
-}); 
+});
 
 
 // toggles loginForm class visibilty
-$(document).ready(function() 
-{
-    $("#loginFormButton").click(function()
-    {
-        if($(".loginForm").is(":visible"))
-        {
-            $(".loginForm").hide(750)
-        }
-        else
-        {
-            $(".loginForm").show(750)
-            $(".drpdown").fadeOut(750)
+$(document).ready(function () {
+    $("#loginFormButton, .closeLoginForm").click(function () {
+        if ($(".loginForm").is(":visible")) {
+            $(".loginForm").hide(500)
+        } else {
+            $(".loginForm").show(500)
+            $(".drpdown").fadeOut(500)
         }
     });
 });
 
 
-$(document).ready(function()
-{
-    $('#categoriesButton').click(function()
-    {
-        if($('.drpdown').is(':visible'))
-        {
+$(document).ready(function () {
+    $('#categoriesButton').click(function () {
+        if ($('.drpdown').is(':visible')) {
             $('.drpdown').fadeOut(300);
-        }else
-        {
+        } else {
             $('.drpdown').fadeIn(300);
         };
     });
 });
 
-$(document).ready(function()
-{
-    $("#shooter").click(function()
-    {
-        $(".horrorGame").hide(500)
-        $(".shooterGame").show(500)
-    }),
-    $("#horror").click(function()
-    {
-        $(".shooterGame").hide(500);
-        $(".horrorGame").show(500);
-    }),
-    $("#all").click(function()
-    {
-        $('.horrorGame, .shooterGame').show(500);
-    });
+$(document).ready(function () {
+    $("#shooter").click(function () {
+            $(".horrorGame").hide(500)
+            $(".shooterGame").show(500)
+        }),
+        $("#horror").click(function () {
+            $(".shooterGame").hide(500);
+            $(".horrorGame").show(500);
+        }),
+        $("#all").click(function () {
+            $('.horrorGame, .shooterGame').show(500);
+        });
 });
 
-window.onload = function () 
-{  
+window.onload = function () {
     setTimeout(function () {
         let x = document.querySelector('.loaderWrap')
         x.remove();
-      }, 2000)
+    }, 2000)
 }
