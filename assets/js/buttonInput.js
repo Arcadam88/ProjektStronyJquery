9c6d2727086b254adc2f4a2591e92a6eb6779412
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2021-08-13 05:13:06 +0200

    Finished subpage template

--- assets/js/buttonInput.js
+++ assets/js/buttonInput.js
@@ -95,24 +95,47 @@ $(document).ready(function () {
             $('.navbar').fadeIn(300);
         })
 });
-let i = 0;
-let images = [];
+var i = 0;
+let imagesCsgo = [];
 
 
-    images[0] = '../assets/image/csgo/image_csgo.webp'
-    images[1] = '../assets/image/csgo/image_csgo1.webp'
-    images[2] = '../assets/image/csgo/image_csgo2.webp'
-    images[3] = '../assets/image/csgo/image_csgo3.webp'
-    images[4] = '../assets/image/csgo/image_csgo4.webp'
-    images[5] = '../assets/image/csgo/image_csgo5.webp'
-    images[6] = '../assets/image/csgo/image_csgo6.webp'
-    images[7] = '../assets/image/csgo/image_csgo7.webp'
+    imagesCsgo[0] = '../assets/image/csgo/image_csgo.webp'
+    imagesCsgo[1] = '../assets/image/csgo/image_csgo1.webp'
+    imagesCsgo[2] = '../assets/image/csgo/image_csgo2.webp'
+    imagesCsgo[3] = '../assets/image/csgo/image_csgo3.webp'
+    imagesCsgo[4] = '../assets/image/csgo/image_csgo4.webp'
+    imagesCsgo[5] = '../assets/image/csgo/image_csgo5.webp'
+    imagesCsgo[6] = '../assets/image/csgo/image_csgo6.webp'
+    imagesCsgo[7] = '../assets/image/csgo/image_csgo7.webp'
   
- 
-    
+ let imagesDoom2016 = [];
+    imagesDoom2016[0] = '../assets/image/doom2016/doom2016_1.webp'
+    imagesDoom2016[1] = '../assets/image/doom2016/doom2016_2.webp'
+    imagesDoom2016[2] = '../assets/image/doom2016/doom2016_3.webp'
+    imagesDoom2016[3] = '../assets/image/doom2016/doom2016_4.webp'
+    imagesDoom2016[4] = '../assets/image/doom2016/doom2016_5.webp'
+    imagesDoom2016[5] = '../assets/image/doom2016/doom2016_6.webp'
+    imagesDoom2016[6] = '../assets/image/doom2016/doom2016_7.webp'
+    imagesDoom2016[7] = '../assets/image/doom2016/doom2016_8.webp'
+    imagesDoom2016[8] = '../assets/image/doom2016/doom2016_9.webp'
+    imagesDoom2016[9] = '../assets/image/doom2016/doom2016_10.webp'
+    imagesDoom2016[10] = '../assets/image/doom2016/doom2016_11.webp'
+    imagesDoom2016[11] = '../assets/image/doom2016/doom2016_thumbnail.webp'
+   
   window.addEventListener('load', function changeImage(){  
-    document.slide.src = images[i];
-    if(i<images.length - 1){
+    document.slideCsgo.src = imagesCsgo[i];
+   
+    if(i<imagesCsgo.length - 1){
+        i++;
+    }else{
+        i=0;
+    }
+    setTimeout(changeImage, 1500);
+});  
+window.addEventListener('load', function changeImage(){  
+    document.slideDoom2016.src = imagesDoom2016[i];
+   
+    if(i<imagesDoom2016.length - 1){
         i++;
     }else{
         i=0;
