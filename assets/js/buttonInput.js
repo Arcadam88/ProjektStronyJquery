e6d340508e269a9945818eb4056a924696228f19
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2021-08-20 23:29:42 +0200

    Version 2.0

--- assets/js/buttonInput.js
+++ assets/js/buttonInput.js
@@ -18,18 +18,9 @@ $(document).ready(function () {
 });
 
 
-//button hiding or displaying button group with "games" button etc.
-$(document).ready(function () {
-    {
-        $(".btn-group").hide();
-    }
-    $("#menu").click(function () {
-        $(".btn-group").fadeToggle(300);
-    });
-});
 
 
-// toggles loginForm class visibilty
+// toggles loginForm visibilty
 $(document).ready(function () {
     $("#loginFormButton, .closeLoginForm").click(function () {
         if ($(".loginForm, .loginFormSubpageEq").is(":visible")) {
@@ -41,7 +32,7 @@ $(document).ready(function () {
     });
 });
 
-
+//After clicking categoriesButton - dropdown with categories is gonna be displayed
 $(document).ready(function () {
     $('#categoriesButton').click(function () {
         if ($('.dropdown').is(':visible')) {
@@ -52,7 +43,7 @@ $(document).ready(function () {
         };
     });
 });
-
+//After clicking each category like "Shooter" or "Horror" - games with corresponding class will be displayed
 $(document).ready(function () {
     
     $("#shooter").click(function () {
@@ -73,14 +64,14 @@ $(document).ready(function () {
             $('.hoverGame').attr('data-aos', ('fade-up'))
         });
 });
-
+//It's just loading animation component, after 1700ms element with .loaderWrap class is gonna be removed
 window.addEventListener('load', function () {
     setTimeout(function () {
         let x = document.querySelector('.loaderWrap')
         x.remove();
     }, 1700)
 })
-
+//After reaching the bottom of the screen - a "Scroll up button" will pop up
 $(window).scroll(function () {
     if ($(window).scrollTop() + $(window).height() == $(document).height()) {
         if ($('#scrollUp').is(":hidden"))
@@ -92,6 +83,7 @@ $(window).scroll(function () {
 
 
 
+//Navigation bar visibility, after clicking element with ".openNavbar" class - ".navbar" is gonna be displayed to the user
 
 $(document).ready(function () {
     $('.navbar').hide();
@@ -102,8 +94,8 @@ $(document).ready(function () {
             $('.navbar').fadeIn(300);
         })
 });
-var i = 0;
-let imagesCsgo = [];
+var i = 0; //starting point for the loop
+let imagesCsgo = []; //Array of images
 
 
 imagesCsgo[0] = '../assets/image/csgo/image_csgo.webp'
@@ -117,12 +109,13 @@ imagesCsgo[7] = '../assets/image/csgo/image_csgo7.webp'
 
 
 window.addEventListener('load', function changeImage() {
+    if($('#slideCsgo').is(':visible') === true){
     document.slideCsgo.src = imagesCsgo[i];
     if (i < imagesCsgo.length - 1) {
         i++;
     } else {
         i = 0;
-    }
+    }}
     setTimeout(changeImage, 1500);
 });
 let imagesDoom2016 = [];
@@ -138,14 +131,15 @@ imagesDoom2016[8] = '../assets/image/doom2016/doom2016_9.webp'
 imagesDoom2016[9] = '../assets/image/doom2016/doom2016_10.webp'
 imagesDoom2016[10] = '../assets/image/doom2016/doom2016_11.webp'
 imagesDoom2016[11] = '../assets/image/doom2016/doom2016_thumbnail.webp'
-
+//slideshow function, it incremenets number of the index in the array every 1500ms 
 window.addEventListener('load', function changeImage() {
+    if($('#slideDoom2016').is(':visible') === true){
     document.slideDoom2016.src = imagesDoom2016[i];
     if (i < imagesDoom2016.length - 1) {
         i++;
     } else {
         i = 0;
-    }
+    }}
     setTimeout(changeImage, 1500);
 });
 let imagesOutlast = [];
@@ -162,12 +156,13 @@ imagesOutlast[9] = '../assets/image/outlast/outlast_10.webp'
 
 
   window.addEventListener('load', function changeImage() {
+    if($('#slideOutlast').is(':visible') === true){
     document.slideOutlast.src = imagesOutlast[i];
     if (i < imagesOutlast.length - 1) {
         i++;
     } else {
         i = 0;
-    }
+    }}
     setTimeout(changeImage, 1500);
 });
 
@@ -180,6 +175,7 @@ imagesCss[4] = '../assets/image/cssource/cs_source_5.webp'
 imagesCss[5] = '../assets/image/cssource/cs_source_thumbnail.webp'
 
 window.addEventListener('load', function changeImage() {
+    if($('#slideCss').is(':visible') === true)
     document.slideCss.src = imagesCss[i];
     if (i < imagesCss.length - 1) {
         i++;
@@ -202,12 +198,13 @@ imagesCs[9] = '../assets/image/cs16/cs_10.webp'
 
 
   window.addEventListener('load', function changeImage() {
+      if($('#slideCs').is(':visible') === true){
     document.slideCs.src = imagesCs[i];
     if (i < imagesCs.length - 1) {
         i++;
     } else {
         i = 0;
-    }
+    }}
     setTimeout(changeImage, 1500);
 });
 
@@ -223,12 +220,13 @@ imagesDoomEternal[7] = '../assets/image/doom_eternal/doom_eternal_8.webp'
 imagesDoomEternal[8] = '../assets/image/doom_eternal/doom_eternal_9.webp'
 
 window.addEventListener('load', function changeImage() {
+    if($('#slideDoomEternal').is(':visible') === true){
     document.slideDoomEternal.src = imagesDoomEternal[i];
     if (i < imagesDoomEternal.length - 1) {
         i++;
     } else {
         i = 0;
-    }
+    }}
     setTimeout(changeImage, 1500);
 });
 
@@ -244,11 +242,120 @@ imagesDbd[7] = '../assets/image/dbd/dbd_8.webp'
 imagesDbd[8] = '../assets/image/dbd/dbd_9.webp'
 
 window.addEventListener('load', function changeImage() {
+    if($('#slideDbd').is(':visible') === true){
     document.slideDbd.src = imagesDbd[i];
     if (i < imagesDbd.length - 1) {
         i++;
     } else {
         i = 0;
-    }
+    }}
+    setTimeout(changeImage, 1500);
+});
+let imagesResEv = [];
+imagesResEv[0] = '../assets/image/residentev/residentev_1.webp'
+imagesResEv[1] = '../assets/image/residentev/residentev_2.webp'
+imagesResEv[2] = '../assets/image/residentev/residentev_3.webp'
+imagesResEv[3] = '../assets/image/residentev/residentev_4.webp'
+imagesResEv[4] = '../assets/image/residentev/residentev_5.webp'
+imagesResEv[5] = '../assets/image/residentev/residentev_6.webp'
+
+
+window.addEventListener('load', function changeImage() {
+    if($('#slideResEv').is(':visible') === true){
+    document.slideResEv.src = imagesResEv[i];
+    if (i < imagesResEv.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }}
+    setTimeout(changeImage, 1500);
+});
+let imagesBw = [];
+imagesBw[0] = '../assets/image/blairwitch/bw_1.webp'
+imagesBw[1] = '../assets/image/blairwitch/bw_2.webp'
+imagesBw[2] = '../assets/image/blairwitch/bw_3.webp'
+imagesBw[3] = '../assets/image/blairwitch/bw_4.webp'
+imagesBw[4] = '../assets/image/blairwitch/bw_5.webp'
+imagesBw[5] = '../assets/image/blairwitch/bw_6.webp'
+imagesBw[6] = '../assets/image/blairwitch/bw_7.webp'
+
+
+window.addEventListener('load', function changeImage() {
+    if($('#slideBw').is(':visible') === true){
+    document.slideBw.src = imagesBw[i];
+    if (i < imagesBw.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }}
+    setTimeout(changeImage, 1500);
+});
+let imagesEw = [];
+imagesEw[0] = '../assets/image/evilwithin/ew_1.webp'
+imagesEw[1] = '../assets/image/evilwithin/ew_2.webp'
+imagesEw[2] = '../assets/image/evilwithin/ew_3.webp'
+imagesEw[3] = '../assets/image/evilwithin/ew_4.webp'
+imagesEw[4] = '../assets/image/evilwithin/ew_5.webp'
+imagesEw[5] = '../assets/image/evilwithin/ew_6.webp'
+imagesEw[6] = '../assets/image/evilwithin/ew_7.webp'
+imagesEw[7] = '../assets/image/evilwithin/ew_8.webp'
+imagesEw[8] = '../assets/image/evilwithin/ew_9.webp'
+
+
+
+window.addEventListener('load', function changeImage() {
+    if($('#slideEw').is(':visible') === true){
+    document.slideEw.src = imagesEw[i];
+    if (i < imagesEw.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }}
+    setTimeout(changeImage, 1500);
+});
+
+let imagesSs4 = [];
+imagesSs4[0] = '../assets/image/serioussam4/ss4_1.webp'
+imagesSs4[1] = '../assets/image/serioussam4/ss4_2.webp'
+imagesSs4[2] = '../assets/image/serioussam4/ss4_3.webp'
+imagesSs4[3] = '../assets/image/serioussam4/ss4_4.webp'
+imagesSs4[4] = '../assets/image/serioussam4/ss4_5.webp'
+imagesSs4[5] = '../assets/image/serioussam4/ss4_6.webp'
+imagesSs4[6] = '../assets/image/serioussam4/ss4_7.webp'
+
+
+
+window.addEventListener('load', function changeImage() {
+    if($('#slideSs4').is(':visible') === true){
+    document.slideSs4.src = imagesSs4[i];
+    if (i < imagesSs4.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }}
+    setTimeout(changeImage, 1500);
+});
+
+let imagesSs3 = [];
+imagesSs3[0] = '../assets/image/serioussam3/ss3_1.webp'
+imagesSs3[1] = '../assets/image/serioussam3/ss3_2.webp'
+imagesSs3[2] = '../assets/image/serioussam3/ss3_3.webp'
+imagesSs3[3] = '../assets/image/serioussam3/ss3_4.webp'
+imagesSs3[4] = '../assets/image/serioussam3/ss3_5.webp'
+imagesSs3[5] = '../assets/image/serioussam3/ss3_6.webp'
+imagesSs3[6] = '../assets/image/serioussam3/ss3_7.webp'
+imagesSs3[7] = '../assets/image/serioussam3/ss3_8.webp'
+imagesSs3[8] = '../assets/image/serioussam3/ss3_9.webp'
+
+
+
+window.addEventListener('load', function changeImage() {
+    if($('#slideSs3').is(':visible') === true){
+    document.slideSs3.src = imagesSs3[i];
+    if (i < imagesSs3.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }}
     setTimeout(changeImage, 1500);
 });
